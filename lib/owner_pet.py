4e4fcea7f1bc0765cd40bb2b694ef543f0ec2261
Will Cooley <william.cooley@gmail.coom>
2023-12-07 11:30:46 -0500

    complete lab

--- lib/owner_pet.py
+++ lib/owner_pet.py
@@ -1,5 +1,41 @@
 class Pet:
-    pass
+
+    PET_TYPES = ["dog", "cat", "rodent", "bird", "reptile", "exotic"]
+
+    all = []
+
+    def __init__(self, name, pet_type, owner=None):
+        self.name = name
+        self.owner = owner
+
+        if pet_type in Pet.PET_TYPES:
+            self.pet_type = pet_type
+            Pet.all.append(self)
+        else:
+            raise Exception("must be in PET-TYPES")
 
 class Owner:
-    pass
+    def __init__(self, name):
+        self.name = name
+
+    def pets(self):
+        owners_pets = [pet for pet in Pet.all if pet.owner == self]
+        return owners_pets
+    
+    def add_pet(self, pet):
+        if isinstance(pet, Pet):
+            pet.owner = self
+        else:
+            raise Exception('must be a pet')
+        
+    def get_sorted_pets(self):
+        sorted_pets = Pet.all
+        sorted_pets = sorted(sorted_pets, key=lambda pet:pet.name)
+        return sorted_pets
+
+        
+
+
+
+# dog1 = Pet('will', 'dog', 'boss')
+# print(Pet.all)
